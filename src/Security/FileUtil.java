febf2a7abce662fc0ab82f523abcb8aa173be408
Tijana <Tijana@Tisha>
2016-06-15 21:31:10 +0200

    dragance

--- src/Security/FileUtil.java
+++ src/Security/FileUtil.java
@@ -1,7 +1,6 @@
 package Security;
 
 import Bean.CertificateWrapper;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
